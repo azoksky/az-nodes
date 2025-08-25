bf01ff4b5f3e13b895fde044ccfee7745f619db2
Ashok K P <62461404+azoksky@users.noreply.github.com>
2025-08-25 10:01:15 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
 
 #from .generate_clip_prompt_node import GenerateCLIPPromptNode
 from .extra_node import AzInput, OverrideCLIPDevice, FluxResolutionNode, GetImageSizeRatio, OverrideVAEDevice, OverrideMODELDevice,PurgeVRAM, PurgeVRAM_V2, AnyType
+from .path_uploader import UploadToAnyPath, add_routes
+
+
 
 NODE_CLASS_MAPPINGS = {
     #"GenerateCLIPPromptNode": GenerateCLIPPromptNode,
@@ -12,6 +15,7 @@ NODE_CLASS_MAPPINGS = {
     "GetImageSizeRatio": GetImageSizeRatio,
     "PurgeVRAM_V1": PurgeVRAM,
     "PurgeVRAM_V2": PurgeVRAM_V2,
+    "UploadToAnyPath": UploadToAnyPath
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -24,4 +28,12 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "GetImageSizeRatio": "Get Image Size Ratio",
     "PurgeVRAM": "Purge VRAM V1",
     "PurgeVRAM_V2": "Purge VRAM V2",
+    "UploadToAnyPath": "Upload → Any Folder Path"
+    
 }
+
+WEB_DIRECTORY = "./js"
+add_routes()
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
+
