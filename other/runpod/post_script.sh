287979d86a49ba76e6b07ca6118705d9859a5a70
Ashok K P <62461404+azoksky@users.noreply.github.com>
2025-08-25 18:06:51 +0530

    Update post_script.sh

--- other/runpod/post_script.sh
+++ other/runpod/post_script.sh
@@ -1,1 +1,16 @@
-echo "Yahoooooo it;s woring."
+#!/bin/bash
+set -e
+
+PY_URL="https://raw.githubusercontent.com/azoksky/az-nodes/main/other/runpod/prepare_comfy.py"
+PY_DEST="/tmp/prepare_comfy.py"
+
+echo "Downloading prepare_comfy.py from $PY_URL"
+curl -fsSL "$PY_URL" -o "$PY_DEST"
+
+if [[ -s "$PY_DEST" ]]; then
+    echo "Executing prepare_comfy.py ..."
+    python3 "$PY_DEST"
+else
+    echo "Download failed or file is empty!"
+    exit 1
+fi
