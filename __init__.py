85fa94f18742480d12d7511306a23f4ab91dff83
Ashok K P <62461404+azoksky@users.noreply.github.com>
2025-08-25 09:46:13 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,27 @@
+
+#from .generate_clip_prompt_node import GenerateCLIPPromptNode
+from .extra_node import AzInput, OverrideCLIPDevice, FluxResolutionNode, GetImageSizeRatio, OverrideVAEDevice, OverrideMODELDevice,PurgeVRAM, PurgeVRAM_V2, AnyType
+
+NODE_CLASS_MAPPINGS = {
+    #"GenerateCLIPPromptNode": GenerateCLIPPromptNode,
+    "AzInput": AzInput,
+    "OverrideCLIPDevice": OverrideCLIPDevice,
+    "OverrideVAEDevice": OverrideVAEDevice,
+    "OverrideMODELDevice": OverrideMODELDevice,
+    "FluxResolutionNode": FluxResolutionNode,
+    "GetImageSizeRatio": GetImageSizeRatio,
+    "PurgeVRAM_V1": PurgeVRAM,
+    "PurgeVRAM_V2": PurgeVRAM_V2,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    # "GenerateCLIPPromptNode": "Generate CLIP Prompt",
+    "AzInput": "Input String",
+    "OverrideCLIPDevice": "Force/Set CLIP Device",
+    "OverrideVAEDevice": "Force/Set VAE Device",
+    "OverrideMODELDevice": "Force/Set MODEL Device",
+    "FluxResolutionNode": "Flux Resolution Calc",
+    "GetImageSizeRatio": "Get Image Size Ratio",
+    "PurgeVRAM": "Purge VRAM V1",
+    "PurgeVRAM_V2": "Purge VRAM V2",
+}
