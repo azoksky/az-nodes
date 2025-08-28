33f956c9c6349b65e0ff74db1e6780d30987ddd9
Ashok K P <62461404+azoksky@users.noreply.github.com>
2025-08-28 21:33:44 +0530

    Update post_script.sh

--- other/runpod/post_script.sh
+++ other/runpod/post_script.sh
@@ -8,19 +8,15 @@ mkdir -p "$WORKSPACE"
 
 # URLs
 PY_URL="https://raw.githubusercontent.com/azoksky/az-nodes/refs/heads/main/other/runpod/prepare_comfy.py"
-LIST_URL="https://raw.githubusercontent.com/azoksky/az-nodes/refs/heads/main/other/runpod/download_list.txt"
 
-# Destinations
+# Destination
 PY_DEST="$WORKSPACE/prepare_comfy.py"
-LIST_DEST="$WORKSPACE/download_list.txt"
 
 # Download files
 curl -fsSL "$PY_URL"   -o "$PY_DEST"
-curl -fsSL "$LIST_URL" -o "$LIST_DEST"
 
 # Normalize line endings (Windows CRLF → Unix LF)
 sed -i 's/\r$//' "$PY_DEST"
-sed -i 's/\r$//' "$LIST_DEST"
 
 # Run the Python script (so exit code is from Python)
-exec python3 -u "$PY_DEST" "$@"
+python3 -u "$PY_DEST" "$@"
