f86c9d8934c55c0508b7aebb84b25875673b1ed4
Ashok K P <62461404+azoksky@users.noreply.github.com>
2025-08-29 17:23:05 +0530

    Update post_script.sh

--- other/runpod/post_script.sh
+++ other/runpod/post_script.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+ensure_full_env_for_ssh() 
+{
+tr '\0' '\n' < /proc/1/environ | awk '{
+  name=$0; sub(/=.*/, "", name);
+  val=$0;  sub(/^[^=]*=/, "", val);
+  gsub(/\\/, "\\\\", val); gsub(/"/, "\\\"", val);
+  printf("if [ -z \"${%s+x}\" ]; then export %s=\"%s\"; fi\n", name, name, val)
+}' > /etc/profile.d/10-container-env.sh
+}
+
+
 COMFYUI_PATH="${COMFYUI_PATH:-/workspace/ComfyUI}"
 WORKSPACE="$(dirname "$COMFYUI_PATH")"
 PY_URL="https://raw.githubusercontent.com/azoksky/az-nodes/refs/heads/main/other/runpod/prepare_comfy.py"
